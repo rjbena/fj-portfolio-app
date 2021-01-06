c651a3399c5210f8409287c27099588de4a06590
ricky <rjbenavides@cpp.edu>
2021-01-06 12:07:38 -0800

    finishing touches

--- components/BasePage.js
+++ components/BasePage.js
@@ -1,10 +1,52 @@
 import { Container } from "reactstrap";
+import { useRouter } from "next/router";
+import Head from "next/head";
 
-const BasePage = ({ className, children }) => {
+const BasePage = ({ className, children, canonicalPath }) => {
+  const router = useRouter();
   return (
-    <div className={`base-page ${className}`}>
-      <Container>{children}</Container>
-    </div>
+    <>
+      <Head>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta
+          name="description"
+          key="description"
+          content="Just a simple portfolio page based on a udemy course by Filipe Jerga"
+        ></meta>
+        <meta property="og:title" key="og:title" content="title" />
+        <meta property="og:local" key="og:locale" content="en_US" />
+        <meta
+          property="og:url"
+          key="og:url"
+          content={`${process.env.BASE_URL}${router.asPath}`}
+        />
+        <meta property="og:type" key="og:type" content="website" />
+        <meta
+          property="og:description"
+          key="og:description"
+          content="Just a simple portfolio page based on a udemy course by Filipe Jerga"
+        />
+        <meta
+          property="og:image"
+          key="og:image"
+          content={`${process.env.BASE_URL}/images/section-1.png`}
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;700&display=swap"
+          rel="stylesheet"
+        ></link>
+        <link
+          rel="canonical"
+          href={`${process.env.BASE_URL}${
+            canonicalPath ? canonicalPath : router.asPath
+          }`}
+        />
+        <title>My Next.JS Portfolio</title>
+      </Head>
+      <div className={`base-page ${className}`}>
+        <Container>{children}</Container>
+      </div>
+    </>
   );
 };
 
--- pages/portfolios/[id]/index.js
+++ pages/portfolios/[id]/index.js
@@ -4,7 +4,21 @@ const Portfolio = ({ portfolio }) => {
   return (
     <div>
       <div>
-        <h1>Portfolio Details</h1>
+        <div className="portfolio-detail">
+          <div class="cover-container d-flex h-100 p-3 mx-auto flex-column">
+            <main role="main" class="inner page-cover">
+              <h1 class="cover-heading">Title</h1>
+              <p class="lead dates">dates</p>
+              <p class="lead info mb-0">jobTitle | company | location</p>
+              <p class="lead">description</p>
+              <p class="lead">
+                <a href="#" class="btn btn-lg btn-secondary">
+                  Visit Company
+                </a>
+              </p>
+            </main>
+          </div>
+        </div>
       </div>
     </div>
   );
